64fc8e679e6319bc1f325363cba424cfe2c6d54d
Rajdev-ullash <rajdevullash111@gmail.com>
2023-09-11 21:28:05 +0600

    Project setup done

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,73 @@
+import { AcademicSemester } from '@prisma/client';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { AcademicSemesterFilterableFields } from './academicSemester.constants';
+import { AcademicSemesterService } from './academicSemester.service';
+
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.insertIntoDB(req.body);
+  sendResponse<AcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester created successfully',
+    data: result,
+  });
+});
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, AcademicSemesterFilterableFields);
+
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+  const result = await AcademicSemesterService.getAllFromDB(filters, options);
+  sendResponse<AcademicSemester[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getDataById = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.getDataById(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester data fetched!!',
+    data: result,
+  });
+});
+
+const updateOneInDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await AcademicSemesterService.updateOneInDB(id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semster updated successfully',
+    data: result,
+  });
+});
+
+const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await AcademicSemesterService.deleteByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester delete successfully',
+    data: result,
+  });
+});
+
+export const AcademicSemesterController = {
+  insertIntoDB,
+  getAllFromDB,
+  getDataById,
+  updateOneInDB,
+  deleteByIdFromDB,
+};
---
+++ src/app/modules/faculty/faculty.constants.ts
@@ -0,0 +1,32 @@
+// Define your constants here
+export const facultyFilterableFields: string[] = [
+  'searchTerm',
+  'facultyId',
+  'email',
+  'contactNo',
+  'gender',
+  'bloodGroup',
+  'gender',
+  'designation',
+  'academicFacultyId',
+  'academicDepartmentId',
+];
+
+export const facultySearchableFields: string[] = [
+  'firstName',
+  'lastName',
+  'middleName',
+  'email',
+  'contactNo',
+  'facultyId',
+  'designation',
+];
+
+export const facultyRelationalFields: string[] = [
+  'academicFacultyId',
+  'academicDepartmentId',
+];
+export const facultyRelationalFieldsMapper: { [key: string]: string } = {
+  academicFacultyId: 'academicFaculty',
+  academicDepartmentId: 'academicDepartment',
+};
